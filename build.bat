48148f65f5175c7a428d1f1e2b5942174e92ed5a
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2025-05-29 13:10:11 -0600

    fixed the build script

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
-cd sound
-compile_sounds.bat
-cd ..
-asm68k /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ /p sonic1.asm, s1built.bin
 
+cd sound
+call compile_sounds
+cd ..
+
+asm68k /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ /p sonic1.asm, s1built.bin
 pause
 start s1built.bin
