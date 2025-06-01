1bba391c6e8498154db88c625395ae798c886dfc
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2025-06-01 10:11:45 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,5 @@
 @echo off
-rem RELEASE BUILD
-asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p sonic1.asm, s1built.bin, s1built.sym, sonic1.lst
-convsym.exe s1built.sym s1built.bin -a
-
-rem DEBUG BUILD
-asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p /e __DEBUG__=1 sonic1.asm, s1built.debug.bin, s1built.debug.sym, s1built.debug.lst
-convsym.exe s1built.debug.sym s1built.debug.bin -a
-
+asm68k /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ /p sonic1.asm, s1built.bin
 pause
 
 if exist s1built.bin (
