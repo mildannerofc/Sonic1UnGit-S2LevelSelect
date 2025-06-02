23613105c56bab337dff75690e94b25d21acd891
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2025-06-02 13:52:46 -0600

    move convsym

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 rem RELEASE BUILD
 asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p sonic1.asm, s1built.bin, s1built.sym, sonic1.lst
-convsym.exe s1built.sym s1built.bin -a
+_debug/convsym.exe s1built.sym s1built.bin -a
 
 rem DEBUG BUILD
 asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p /e __DEBUG__=1 sonic1.asm, s1built.debug.bin, s1built.debug.sym, s1built.debug.lst
-convsym.exe s1built.debug.sym s1built.debug.bin -a
+_debug/convsym.exe s1built.debug.sym s1built.debug.bin -a
 pause
 
 if exist s1built.bin (
